8f16311b4dc993c786669174e2e3c08b150e3b6e
francine-godoi <francine.godoi@gmail.com>
2025-01-11 09:55:22 -0300

    formatação dos arquivos

--- app_sql/models/tarefas.py
+++ app_sql/models/tarefas.py
@@ -1,8 +1,18 @@
 from datetime import datetime
 
+
 class Tarefa:
-    
-    def __init__(self, id_usuario: int, descricao: str, importancia: str, id_tarefa: int = None, status: str = 'A', criado_em: str = datetime.today().strftime('%d/%m/%Y'), finalizado_em: str = None) -> None:
+
+    def __init__(
+        self,
+        id_usuario: int,
+        descricao: str,
+        importancia: str,
+        id_tarefa: int = None,
+        status: str = "A",
+        criado_em: str = datetime.today().strftime("%d/%m/%Y"),
+        finalizado_em: str = None,
+    ) -> None:
         self.id_tarefa = id_tarefa
         self.descricao = descricao
         self.importancia = importancia
@@ -10,7 +20,6 @@ class Tarefa:
         self.criado_em = criado_em
         self.finalizado_em = finalizado_em
         self.id_usuario = id_usuario
-
 
     def pegar_info_tarefa(self) -> tuple:
         return (self.id_usuario, self.descricao, self.importancia)
--- app_sql/repositories/repositorio_tarefas.py
+++ app_sql/repositories/repositorio_tarefas.py
@@ -1,6 +1,8 @@
+from datetime import date
+
 from models.tarefas import Tarefa
 from repositories.auxiliar_db import AuxiliarDB
-from datetime import date
+
 
 class RepositorioTarefa(AuxiliarDB):
 
@@ -11,7 +13,6 @@ class RepositorioTarefa(AuxiliarDB):
         self.conexao = None
         self.cursor = None
         self.criar_tabela_tarefas()
-
 
     def criar_tabela_tarefas(self) -> None:
 
@@ -28,22 +29,27 @@ class RepositorioTarefa(AuxiliarDB):
 
         self.executar_sql(sql)
         self.fechar_conexao()
-    
 
     def adicionar_tarefa(self, tarefa: Tarefa) -> int:
 
         sql = f"""INSERT INTO {self.__NOME_TABELA} (id_usuario, descricao, importancia, status, criado_em) VALUES(?,?,?,?,?)"""
-        
+
         data = date.today().strftime("%d/%m/%Y")
-        info_tarefa = (tarefa.id_usuario, tarefa.descricao, tarefa.importancia, "A", data)
+        info_tarefa = (
+            tarefa.id_usuario,
+            tarefa.descricao,
+            tarefa.importancia,
+            "A",
+            data,
+        )
         resultado = self.executar_sql(sql, info_tarefa, comitar=True).rowcount
         self.fechar_conexao()
 
         return resultado
 
+    def editar_tarefa(self, tarefa: Tarefa) -> int:
 
-    def editar_tarefa(self, tarefa:Tarefa) -> int:
-
+        # trunk-ignore(bandit/B608)
         sql = f"""UPDATE {self.__NOME_TABELA} SET descricao = ?, importancia = ? WHERE id_tarefa = ? AND status = 'A'"""
 
         info_tarefa = (tarefa.descricao, tarefa.importancia, tarefa.id_tarefa)
@@ -52,19 +58,21 @@ class RepositorioTarefa(AuxiliarDB):
 
         return resultado
 
-    
-    def excluir_tarefa(self, id_tarefa:int) -> int:
+    def excluir_tarefa(self, id_tarefa: int) -> int:
 
-        sql = f"""DELETE FROM {self.__NOME_TABELA} WHERE id_tarefa = ? AND status = 'A'"""
+        sql = (
+            # trunk-ignore(bandit/B608)
+            f"""DELETE FROM {self.__NOME_TABELA} WHERE id_tarefa = ? AND status = 'A'"""
+        )
 
         resultado = self.executar_sql(sql, (id_tarefa,), comitar=True).rowcount
         self.fechar_conexao()
 
         return resultado
 
-        
-    def finalizar_tarefa(self, id_tarefa:int) -> int:
+    def finalizar_tarefa(self, id_tarefa: int) -> int:
 
+        # trunk-ignore(bandit/B608)
         sql = f"""UPDATE {self.__NOME_TABELA} SET status = 'F', finalizado_em = ? WHERE id_tarefa = ? AND status = 'A'"""
 
         data = date.today().strftime("%d/%m/%Y")
@@ -73,22 +81,24 @@ class RepositorioTarefa(AuxiliarDB):
 
         return resultado
 
-
     def selecionar_todas_tarefas(self, id_usuario: int) -> list[Tarefa]:
 
+        # trunk-ignore(bandit/B608)
         sql = f"""SELECT id_tarefa, descricao, importancia, status, criado_em, finalizado_em FROM {self.__NOME_TABELA} WHERE id_usuario = ? ORDER BY id_tarefa"""
 
         resultados = self.executar_sql(sql, (id_usuario,)).fetchall()
         self.fechar_conexao()
 
-        lista_tarefas = lista_tarefas = self.criar_lista_de_tarefas(id_usuario, resultados)
-        
+        lista_tarefas = lista_tarefas = self.criar_lista_de_tarefas(
+            id_usuario, resultados
+        )
+
         return lista_tarefas
-    
 
     def selecionar_tarefa_por_id(self, id_tarefa: int, id_usuario: int) -> Tarefa:
-        """ retorna id_usuario, descricao, importancia, status """
+        """retorna id_usuario, descricao, importancia, status"""
 
+        # trunk-ignore(bandit/B608)
         sql = f"""SELECT id_usuario, descricao, importancia, status, id_tarefa FROM {self.__NOME_TABELA} WHERE id_tarefa = ? AND status = 'A' and id_usuario = ? ORDER BY id_tarefa"""
 
         resultado = self.executar_sql(sql, (id_tarefa, id_usuario)).fetchone()
@@ -96,55 +106,74 @@ class RepositorioTarefa(AuxiliarDB):
 
         if not resultado:
             return 0
-        
+
         tarefa = Tarefa(id_usuario, resultado[1], resultado[2], id_tarefa)
-        
+
         return tarefa
 
+    def selecionar_tarefas_por_status(
+        self, id_usuario: int, status: str
+    ) -> list[Tarefa]:
+        """retorna id_tarefa, descricao, importancia"""
 
-    def selecionar_tarefas_por_status(self, id_usuario: int, status: str) -> list[Tarefa]:
-        """ retorna id_tarefa, descricao, importancia """
-
+        # trunk-ignore(bandit/B608)
         sql = f"""SELECT id_tarefa, descricao, importancia, status, criado_em, finalizado_em FROM {self.__NOME_TABELA} WHERE id_usuario = ? AND status = ? ORDER BY id_tarefa"""
 
         resultados = self.executar_sql(sql, (id_usuario, status)).fetchall()
         self.fechar_conexao()
 
-        lista_tarefas = lista_tarefas = self.criar_lista_de_tarefas(id_usuario, resultados)
-        
+        lista_tarefas = lista_tarefas = self.criar_lista_de_tarefas(
+            id_usuario, resultados
+        )
+
         return lista_tarefas
-    
 
-    def selecionar_tarefas_por_importancia(self, id_usuario: int, importancia: str) -> list[Tarefa]:
-        """ retorna id_tarefa, descricao, importancia, status, criado_em, finalizado_em """
+    def selecionar_tarefas_por_importancia(
+        self, id_usuario: int, importancia: str
+    ) -> list[Tarefa]:
+        """retorna id_tarefa, descricao, importancia, status, criado_em, finalizado_em"""
 
+        # trunk-ignore(bandit/B608)
         sql = f"""SELECT id_tarefa, descricao, importancia, status, criado_em, finalizado_em FROM {self.__NOME_TABELA} WHERE id_usuario = ? AND importancia = ? ORDER BY id_tarefa"""
 
         resultados = self.executar_sql(sql, (id_usuario, importancia)).fetchall()
         self.fechar_conexao()
 
-        lista_tarefas = lista_tarefas = self.criar_lista_de_tarefas(id_usuario, resultados)
-        
+        lista_tarefas = lista_tarefas = self.criar_lista_de_tarefas(
+            id_usuario, resultados
+        )
+
         return lista_tarefas
-    
 
-    def selecionar_tarefas_por_data(self, tipo_data: str, id_usuario: int, data_inicio: str, data_final: str) -> list[Tarefa]:
-        """ retorna id_tarefa, descricao, importancia, status, criado_em, finalizado_em """
+    def selecionar_tarefas_por_data(
+        self, tipo_data: str, id_usuario: int, data_inicio: str, data_final: str
+    ) -> list[Tarefa]:
+        """retorna id_tarefa, descricao, importancia, status, criado_em, finalizado_em"""
 
+        # trunk-ignore(bandit/B608)
         sql = f"""SELECT id_tarefa, descricao, importancia, status, criado_em, finalizado_em FROM {self.__NOME_TABELA} WHERE id_usuario = ? AND {tipo_data} >= ? AND {tipo_data} <= ? ORDER BY id_tarefa"""
 
-        resultados = self.executar_sql(sql, (id_usuario, data_inicio, data_final)).fetchall()
+        resultados = self.executar_sql(
+            sql, (id_usuario, data_inicio, data_final)
+        ).fetchall()
         self.fechar_conexao()
 
         lista_tarefas = self.criar_lista_de_tarefas(id_usuario, resultados)
-        
+
         return lista_tarefas
-    
 
     def criar_lista_de_tarefas(self, id_usuario, resultados) -> list[Tarefa]:
         lista_tarefas = []
         for resultado in resultados:
-            tarefa = Tarefa(id_usuario=id_usuario, id_tarefa=resultado[0], descricao=resultado[1], importancia=resultado[2], status=resultado[3], criado_em=resultado[4], finalizado_em=resultado[5])
+            tarefa = Tarefa(
+                id_usuario=id_usuario,
+                id_tarefa=resultado[0],
+                descricao=resultado[1],
+                importancia=resultado[2],
+                status=resultado[3],
+                criado_em=resultado[4],
+                finalizado_em=resultado[5],
+            )
             lista_tarefas.append(tarefa)
-        
+
         return lista_tarefas
