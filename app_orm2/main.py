21d0ca39c693783b1442074a33b78a8f210266ef
Francine <francine.godoi@gmail.com>
2025-01-14 22:09:22 -0300

    #

--- app_orm2/main.py
+++
@@ -1,4 +0,0 @@
-from controllers.controller_menus import ControllerMenus
-
-if __name__ == "__main__":
-    ControllerMenus().main()
--- app_orm2/views/tela_inicial.py
+++
@@ -1,9 +0,0 @@
-class TelaInicial:           
-        
-    def mostrar_menu_inicial(self):        
-
-        print("\nO que deseja fazer?\n")
-        print("1. Cadastrar Novo Usuário.")
-        print("2. Fazer Login.")
-        print("3. Sair do Sistema.\n")
-        
